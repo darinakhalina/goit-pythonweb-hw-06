dae9d6970a3b5c1556faeb369d3b5482a347a9b0
Darina Khalina <darina.khalina@namecheap.com>
2025-03-29 19:55:03 +0200

    feat: add 11 and 12

--- my_select.py
+++ my_select.py
@@ -1,6 +1,12 @@
+import logging
 from sqlalchemy import func
 from connect import session
 from models import Student, Grade, Subject, Group, Teacher
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 def select_1():
@@ -130,67 +136,121 @@ def select_10(student_id: str, teacher_id: str):
     return result
 
 
+def select_11(student_id: str, teacher_id: str):
+    result = (
+        session.query(func.avg(Grade.grade))
+        .join(Subject, Grade.subject_id == Subject.id)
+        .filter(Grade.student_id == student_id, Subject.teacher_id == teacher_id)
+        .scalar()
+    )
+    session.close()
+    return result
+
+
+def select_12(group_name: str, subject_name: str):
+    subquery = (
+        session.query(func.max(Grade.date))
+        .join(Student, Grade.student_id == Student.id)
+        .join(Group, Student.group_id == Group.id)
+        .join(Subject, Grade.subject_id == Subject.id)
+        .filter(Group.name == group_name, Subject.name == subject_name)
+        .scalar_subquery()
+    )
+
+    result = (
+        session.query(Student.name, Grade.grade, Grade.date)
+        .join(Group, Student.group_id == Group.id)
+        .join(Grade, Grade.student_id == Student.id)
+        .join(Subject, Grade.subject_id == Subject.id)
+        .filter(
+            Group.name == group_name,
+            Subject.name == subject_name,
+            Grade.date == subquery,
+        )
+        .all()
+    )
+
+    session.close()
+    return result
+
+
 def main():
     result1 = select_1()
-    print("Top 5 students with the highest average grades:")
+    logger.info("Top 5 students with the highest average grades:")
     for student in result1:
-        print(
+        logger.info(
             f"Student ID: {student.id}, Name: {student.name}, Average Grade: {student.avg_grade}"
         )
 
     subject_name = "Mathematics"
     result2 = select_2(subject_name)
-    print(f"Student with the highest average grade in {subject_name}:")
+    logger.info(f"Student with the highest average grade in {subject_name}:")
     for student in result2:
-        print(
+        logger.info(
             f"Student ID: {student.id}, Name: {student.name}, Average Grade: {student.avg_grade}"
         )
 
     subject_name = "Mathematics"
     result3 = select_3(subject_name)
-    print(f"Average grade in groups for the subject '{subject_name}':")
+    logger.info(f"Average grade in groups for the subject '{subject_name}':")
     for group in result3:
-        print(
+        logger.info(
             f"Group ID: {group.id}, Name: {group.name}, Average Grade: {group.avg_grade:.2f}"
         )
 
     result4 = select_4()
-    print(f"Average grade for the entire course: {result4:.2f}")
+    logger.info(f"Average grade for the entire course: {result4:.2f}")
 
     teacher_name = "Jessica Freeman"
     result5 = select_5(teacher_name)
-    print(f"Courses taught by {teacher_name}:")
+    logger.info(f"Courses taught by {teacher_name}:")
     for subject in result5:
-        print(f"- {subject}")
+        logger.info(f"- {subject}")
 
     group_name = "Group 1"
     result6 = select_6(group_name)
-    print(f"Students in group '{group_name}':")
+    logger.info(f"Students in group '{group_name}':")
     for student in result6:
-        print(f"- {student}")
+        logger.info(f"- {student}")
 
     group_name = "Group 1"
     subject_name = "Mathematics"
     result7 = select_7(group_name, subject_name)
-    print(f"Grades for students in group '{group_name}' for subject '{subject_name}':")
+    logger.info(
+        f"Grades for students in group '{group_name}' for subject '{subject_name}':"
+    )
     for student_name, grade in result7:
-        print(f"Student: {student_name}, Grade: {grade}")
+        logger.info(f"Student: {student_name}, Grade: {grade}")
 
     teacher_name = "Kayla Jones"
     result8 = select_8(teacher_name)
-    print(f"Average grade given by {teacher_name}: {result8}")
+    logger.info(f"Average grade given by {teacher_name}: {result8}")
 
     student_id = "1"
     result9 = select_9(student_id)
     for course in result9:
-        print(course[0])
+        logger.info(course[0])
 
     student_id = "1"
     teacher_id = "1"
     subjects = select_10(student_id, teacher_id)
-    print(f"Courses taught by {teacher_name} for {student_name}:")
+    logger.info(f"Courses taught by {teacher_name} for {student_name}:")
     for subject in subjects:
-        print(subject.name)
+        logger.info(subject.name)
+
+    student_id = "1"
+    teacher_id = "1"
+    result11 = select_11(student_id, teacher_id)
+    logger.info(
+        f"The average grade given by teacher {teacher_id} to student {student_id}: {result11}"
+    )
+
+    group_name = "Group 1"
+    subject_name = "Mathematics"
+    result12 = select_12(group_name, subject_name)
+    logger.info(f"Latest grades in group '{group_name}' for subject '{subject_name}':")
+    for student_name, grade, date in result12:
+        logger.info(f"Student: {student_name}, Grade: {grade}, Date: {date}")
 
 
 if __name__ == "__main__":
